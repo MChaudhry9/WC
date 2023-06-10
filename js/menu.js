37e21c443a480be1250b982d5d126e3965f629ee
Mamuna Chaudhry <128327829+MChaudhry9@users.noreply.github.com>
2023-06-09 23:22:55 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ let flag=getCookie("flag");
 if(flag =="true") 
       document.writeln("<a href='logoff.html'><span class='button'>Logoff</span></a>");
 else 
-    document.writeln("<a href='login.html'><span class='button'>Login</span></a>");	
+    document.writeln("<a href='index.html'><span class='button'>Login</span></a>");	
 
 document.writeln("<a href='about.html'><span class='button'>About Us</span></a>");  
 document.writeln("<a href='products.html'><span class='button'>Inventory</span></a>");	
